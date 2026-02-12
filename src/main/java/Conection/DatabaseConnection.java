c25f0800edb75626a136cec158d86da93125555e
Raimsyden <santi16312vt@gmail.com>
2026-02-12 01:32:08 -0500

    Techsoft V0.1 Conexión y creación de GUI

---
+++ src/main/java/Conection/DatabaseConnection.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Conection;
+
+/**
+ *
+ * @author santi
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/si"; 
+    private static final String USER = "root"; 
+    private static final String PASSWORD = ""; 
+
+    public static Connection getConnection() throws SQLException {
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); 
+        }
+       
+        return DriverManager.getConnection(URL, USER, PASSWORD); 
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = getConnection()) {
+            if (connection != null) {
+                System.out.println("Conexión exitosa a la base de datos.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        }
+    }
+}
